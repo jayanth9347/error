640bcb4ca975d27bfbf7cca45ed57cbaded4bcf5
jayanth <pujalajayanth9347@gmail.com>
2024-05-07 06:09:06 +0000

    commit message

---
+++ src/components/GoogleSuggestions/index.js
@@ -0,0 +1,12 @@
+const GoogleSuggestions = props => {
+  const {suggestionsList} = props
+  const {suggestion, id} = suggestion[0]
+  return (
+    <div>
+      {' '}
+      <h1> Hello </h1>{' '}
+    </div>
+  )
+}
+
+export default GoogleSuggestions
